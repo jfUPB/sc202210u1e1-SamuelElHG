8b39401505d60cc1c9644fc900e1d1c15ed81183
Samuel HG <samuel.hernandezg@upb.edu.co>
2022-02-13 09:45:08 -0500

    Organizamos el principio, solo falta el llenado y la comparación

--- problem/program.asm
+++ problem/program.asm
@@ -3,22 +3,32 @@
 // samuel.hernandezg@upb.edu.co
 (START)
     @START
-    @16384
+    @4
+    D=M
+    @67
+    D=D-A
+    @28
+    D;JEQ
+    //La primera parte del código es para comprobar si los valores iniciales que necesitamos en la RAM están hechos
+    @16384 //primer pixel a pintar
     D=A
     @1
     M=D
-    @24576
+    @24576 // último pixel a pintar
     D=A
-    @2
+    @2 
     M=D
-    @2
+    @2 //valor de la F
     D=A
-    @3
+    @3 //lugar donde está la F
     M=D
-    @67
+    @67 //valor de la C
     D=A
-    @4
+    @4  //espacio donde está la C
     M=D
+    D=0
+    @0
+    D;JEQ
 
 
-    0;JMP
+    
