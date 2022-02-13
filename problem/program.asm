c589d9af50bfaa9672940c2e88686c6169c8bdba
Samuel HG <samuel.hernandezg@upb.edu.co>
2022-02-13 08:58:07 -0500

    son los valores de las letras, y los valores de ram a trabajar

--- problem/program.asm
+++ problem/program.asm
@@ -3,5 +3,22 @@
 // samuel.hernandezg@upb.edu.co
 (START)
     @START
+    @16384
+    D=A
+    @1
+    M=D
+    @24576
+    D=A
+    @2
+    M=D
+    @2
+    D=A
+    @3
+    M=D
+    @67
+    D=A
+    @4
+    M=D
+
 
     0;JMP
